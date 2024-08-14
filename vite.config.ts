b3292f769bdcfc8b67af7fe7d2f0924074b37965
Sivamani-18 <siva@mindwaveventures.com>
2024-08-14 07:46:11 +0530

    fix: base url issued

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv, ConfigEnv, UserConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }: ConfigEnv): UserConfig => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    base:
+      mode === 'production' && process.env.VITE_Environments === 'github'
+        ? process.env.VITE_BASE_URL
+        : '/',
+    plugins: [react()],
+  });
+};
