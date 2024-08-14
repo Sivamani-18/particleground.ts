ec4941efba6abd862f2fcf3933683fa64c5e13b7
Sivamani-18 <siva@mindwaveventures.com>
2024-08-14 08:58:59 +0530

    update env

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ export default ({ mode }: ConfigEnv): UserConfig => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   return defineConfig({
     base:
-      mode === 'production' && process.env.VITE_Environments === 'github'
-        ? process.env.VITE_BASE_URL
+      mode === 'production' && process.env.VITE_APP_Environments === 'github'
+        ? process.env.VITE_APP_BASE_URL
         : '/',
     plugins: [react()],
   });
